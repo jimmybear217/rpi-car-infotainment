b93a010a3a38e8a71baad485bffdec178cac9de8
jimmybear217 <jimmybear217@gmail.com>
2020-03-03 14:17:54 -0800

    removed angular added test gui setup express route

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.get('/', function (req, res) { res.send('Hello World!'); });
+// router
+app.get('/version', function (req, res) { res.send('v0.0.1'); });
+app.get('/', function (req, res) { res.sendFile(path.join(__dirname + '/client/index.html')); });
+app.get('/gui', function (req, res) { res.sendFile(path.join(__dirname + '/client/gui.html')); });
+app.route('*').all(function (req, res) { res.setHeader("Location", "/"); res.sendStatus(303).send("Error 404") });
 
-var server = app.listen(3000, function () { var host = server.address().address; var port = server.address().port;
-
-console.log('Example app listening at http://%s:%s', host, port); });
+// server
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server listening at http://%s:%s', host, port);
+});
