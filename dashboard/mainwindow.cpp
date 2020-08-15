99931dca04d2767abbf1051fa898fce368662254
jimmybear217 <jimmybear217@gmail.com>
2020-08-14 17:28:49 -0700

    learning to interact with QT window and elements

--- dashboard/mainwindow.cpp
+++ dashboard/mainwindow.cpp
@@ -1,15 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <stdio.h>
+#include <string>
+#include <iostream>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+// script on starting
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    printf("Settings up MainWindow\n");
     ui->setupUi(this);
+    Qt::WindowStates state = ui->centralwidget->windowState(); // Qt::WindowFullScreen
+    ui->home_lcdTime->setProperty("value", "123.0");
+    ui->home_progress->setProperty("value", "99");
+    ui->home_html->setProperty("source", "/var/www/html/index.html");
 }
 
-MainWindow::~MainWindow()
-{
+// script on closing
+MainWindow::~MainWindow() {
+    printf("Closing MainWindow");
     delete ui;
 }
-
